feacd5614c3d5195c0b621033d797523f44d3898
John Tyree <johntyree@gmail.com>
2013-02-12 01:18:57 +0100

    How to get a backtrace from inside C/C++.

---
+++ backtrace.h
@@ -0,0 +1,21 @@
+#ifndef BACKTRACE_H
+#define BACKTRACE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Demangle C++ symbols and return ptr to entire result, unbounded. (scary!) */
+inline char* demangle2(char *bt);
+
+/* Demangle C++ symbols in *bt and save n bytes of the result in *dest. */
+inline char* demangle(char *dest, size_t n, char *bt);
+
+/* Write a backtrace of depth size into *buf, return buf */
+inline char* get_backtrace(char *buf, size_t size);
+
+#ifdef __cplusplus
+} /* extern C */
+#endif
+
+#endif /* end of include guard */
