28c3b8519be8519e6618235285a380611e1d40d2
John Tyree <johntyree@gmail.com>
2013-02-22 01:24:09 +0100

    GistID, not Gist. WS.

--- GNUC_47_compat.h
+++ GNUC_47_compat.h
@@ -1,13 +1,11 @@
-// Gist: 4665669
+// GistID: 4665669
 // Header for compilation with GCC-4.7
 
 #if defined(__GNUC__)
-
 #if __GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ > 6)
 
 #undef _GLIBCXX_ATOMIC_BUILTINS
 #undef _GLIBCXX_USE_INT128
 
 #endif
-
 #endif
